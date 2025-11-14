b2ae65b347f65489a479301cf45f156d63980e58
Abdulsamad Ajala <melu2127@gmail.com>
2025-11-14 00:35:22 +0000

    adding Contracts page with mock data and actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import DashboardPage from "./Pages/DashboardPage";
 import SettingsPage from "./Pages/SettingPage";
+import ContractsPage from "./Pages/ContractsPage";
+
 
 const App: React.FC = () => {
   return (
@@ -12,6 +14,7 @@ const App: React.FC = () => {
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/contracts" element={<ContractsPage />} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
---
+++ src/services/mockContracts.ts
@@ -0,0 +1,22 @@
+import { ContractItem } from "../types/contractList";
+
+export const mockContracts: ContractItem[] = [
+  {
+    id: "1",
+    name: "NDA_Acme_Corp.pdf",
+    uploadedAt: "2025-01-05",
+    size: "320 KB",
+  },
+  {
+    id: "2",
+    name: "Supplier_Agreement_Final.docx",
+    uploadedAt: "2025-01-12",
+    size: "540 KB",
+  },
+  {
+    id: "3",
+    name: "Service_Contract_Draft.txt",
+    uploadedAt: "2025-02-01",
+    size: "95 KB",
+  },
+];
